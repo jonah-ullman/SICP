dd06bee8858f50cc0016eb7857738b086e1f8174
Jonah Ullman <jonahmullman@gmail.com>
2020-08-21 11:03:39 -0500

    exercise 1.7)

--- Ex 1.7.rkt
+++ Ex 1.7.rkt
@@ -3,8 +3,6 @@
 
 (define (average x y) (/ (+ x y) 2))
 
-(define (sqrt x) (sqrt-iter 1.0 x))
-
 (define (improve guess x) (average guess (/ x guess)))
 
 (define (good-enough? guess x)
@@ -14,5 +12,20 @@
 guess
 (sqrt-iter (improve guess x) x)))
 
+(define (sqrt x) (sqrt-iter 1.0 x))
+
+(average 1.0 (/ 0.00001 1.0))
+(average 0.500005 (/ 0.00001 0.500005))
+(average 0.250012499900001 (/ 0.00001 0.250012499900001))
+(average 0.1250262489500585 (/ 0.00001 0.1250262489500585))
+(average 0.06255311607712873 (/ 0.00001 0.06255311607712873))
 (square (sqrt 0.00001))
+
+'break
+
+
+(average 1.0 (/ 9876543219876543 1.0))
+(average 4938271609938272.0 (/ 9876543219876543 4938271609938272.0))
+(average 2469135804969137.0 (/ 9876543219876543 2469135804969137.0))
+
 
